865da7d30379a7ddd65ee7cb1a66174c033044ec
Khalil Khalil <khalil.t.khalil@gmail.com>
2023-08-17 12:38:29 -0500

    create barbers table

--- lib/models.py
+++ lib/models.py
@@ -1,4 +1,17 @@
 from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, Time, Date, Integer, String
 
 
 Base = declarative_base()
+
+
+class Barber(Base):
+    __tablename__ = "barbers"
+
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    email = Column(String)
+
+    def __repr__(self):
+        return f"Barber {self.first_name} {self.last_name}"
