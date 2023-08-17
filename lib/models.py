3895de4c1eb0c805a3728b186c09413b21e361db
Khalil Khalil <khalil.t.khalil@gmail.com>
2023-08-17 12:47:40 -0500

    create clients table

--- lib/models.py
+++ lib/models.py
@@ -15,3 +15,15 @@ class Barber(Base):
 
     def __repr__(self):
         return f"Barber {self.first_name} {self.last_name}"
+
+
+class Client(Base):
+    __tablename__ = "clients"
+
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    email = Column(String)
+
+    def __repr__(self):
+        return f"Client {self.first_name} {self.last_name}"
