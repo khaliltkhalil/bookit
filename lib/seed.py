b7588ab747c85c155c73a45e1df9694585a01319
Khalil Khalil <khalil.t.khalil@gmail.com>
2023-08-18 12:01:13 -0500

    fix typo in seed.py

--- lib/seed.py
+++ lib/seed.py
@@ -6,6 +6,6 @@ from sqlalchemy.orm import Session
 
 engine = create_engine("sqlite:///bookit.db")
 
-if __nme__ == "__main__":
+if __name__ == "__main__":
     with Session(engine) as session:
         pass
