e0e9b3bdf311f5e654feebdc1cc6553c075ce45c
Khalil Khalil <khalil.t.khalil@gmail.com>
2023-08-19 20:18:44 -0500

    start writing  barber func

--- lib/bookit.py
+++ lib/bookit.py
@@ -1,6 +1,7 @@
 from models import Barber, Client, Appointment
 from simple_term_menu import TerminalMenu
 from prettycli import red, blue, green
+import re
 
 
 class Bookit:
@@ -10,7 +11,20 @@ class Bookit:
         options = ["Barber", "Client", "Exit"]
         terminal_menu = TerminalMenu(options)
         menu_entry_index = terminal_menu.show()
-        print(f"You have selected {options[menu_entry_index]}!")
+        if menu_entry_index == 0:
+            self.barber()
+
+    def barber(self):
+        print(green("\nPlease enter your email address. type exit to go back\n"))
+        email = input()
+        exp = r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b"
+        if email == "exit":
+            self.start()
+        if re.fullmatch(exp, email):
+            pass
+        else:
+            print(red("email address is not valid\n"))
+            self.barber()
 
 
 cli = Bookit()
